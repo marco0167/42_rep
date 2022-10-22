bde73384776740e09c4fadb33f989459b2363e6b
Marco_Lavoro <m.coppola@software-inside.it>
2022-10-22 20:48:30 +0200

    ex09

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 char	*ft_strlowcase(char *str)
 {
 	int	c;
@@ -28,13 +26,25 @@ char	*ft_strlowcase(char *str)
 
 char	*ft_strcapitalize(char *str)
 {
+	int	c;
+
+	c = 0;
 	str = ft_strlowcase(str);
+	while (str[c] != '\0')
+	{
+		if (str[c] >= 'a' && str[c] <= 'z')
+			if (!(str[c - 1] >= 'a' && str[c - 1] <= 'z'))
+				if (!(str[c - 1] >= 'A' && str[c - 1] <= 'Z'))
+					if (!(str[c - 1] >= '0' && str[c - 1] <= '9'))
+							str[c] -= 32;
+		c++;
+	}
 	return (str);	
 }
-
+/*
 int	main(void)
 {
 	char	str[] = "salut, commWnt tu vas ? 42mots quarante-deux; cinquante+et+un";
 
 	printf("%s", ft_strcapitalize(str));
-}
+}*/
