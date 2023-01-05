5e4d45e97294ac785e99e1b0b3518b05f064d459
marco0167 <marcocoppola01671@gmail.com>
2023-01-05 15:38:02 +0100

    add ft_tolower

--- Corso/libft/ft_tolower.c
+++ Corso/libft/ft_tolower.c
@@ -1,23 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/03 18:40:28 by mcoppola          #+#    #+#             */
-/*   Updated: 2023/01/05 15:37:44 by mcoppola         ###   ########.fr       */
+/*   Created: 2023/01/05 13:26:27 by mcoppola          #+#    #+#             */
+/*   Updated: 2023/01/05 15:36:16 by mcoppola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+// #include <ctype.h>
+// #include <stdio.h>
+int	ft_tolower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	else
+		return (c);
+}
 
-int	ft_isdigit(int c);
-int	ft_isalpha(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_tolower(int c);
-
-#endif
+// int	main(void)
+// {
+// 	printf("%c", ft_tolower('A'));
+// 	printf("%c", tolower('A'));
+// }
