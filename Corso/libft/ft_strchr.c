6b9afe67eeffeafbccd14f1a49ce3b7d7ef4afe4
marco0167 <marcocoppola01671@gmail.com>
2023-01-06 19:04:02 +0100

    removed comment

--- Corso/libft/ft_strchr.c
+++ Corso/libft/ft_strchr.c
@@ -6,20 +6,15 @@
 /*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 16:54:04 by mcoppola          #+#    #+#             */
-/*   Updated: 2023/01/06 17:34:14 by mcoppola         ###   ########.fr       */
+/*   Updated: 2023/01/06 19:03:52 by mcoppola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <string.h>
-#include <stdio.h>
 
 char	*ft_strchr(char *s, int c)
 {
 	int	i;
 
 	i = 0;
-	// if (c == '\0')
-	// 	return ('\0');
 	while (s[i] != c && s[i])
 	{
 		i++;
@@ -27,13 +22,4 @@ char	*ft_strchr(char *s, int c)
 	if (s[i] == c)
 		return (s + i);
 	return ("(null)");
-}
-
-int	main(void)
-{
-	char	*s;
-
-	s = "ciaoo";
-	// printf("%s\n", strchr(s, '5'));
-	printf("%s\n", ft_strchr(s, '5'));
 }
