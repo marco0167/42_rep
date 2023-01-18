feb7d5d777546a6fc3ec1194b413d99c23a9c5b5
marco0167 <marcocoppola01671@gmail.com>
2023-01-18 19:30:38 +0100

    add memchr

---
+++ Corso/libft/ft_memchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 18:04:58 by mcoppola          #+#    #+#             */
+/*   Updated: 2023/01/18 18:37:29 by mcoppola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memchr(const void *s, int c, int n)
+{
+	int				i;
+	const	char	*char_s;
+
+	char_s = (const char *)s;
+	i = 0;
+	while (i < n && char_s[i])
+	{
+		if (char_s[i] == c)
+			return (((void *)char_s) + i);
+		i++;
+	}
+	return ((void *)0);
+
+}
