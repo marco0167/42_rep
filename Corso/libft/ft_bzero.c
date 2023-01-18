7bfba6d3f9fb640fbfd5e61e93ae0e3b767dbde4
marco0167 <marcocoppola01671@gmail.com>
2023-01-18 19:30:24 +0100

    edit bzero (call ft_memset inside)

--- Corso/libft/ft_bzero.c
+++ Corso/libft/ft_bzero.c
@@ -6,15 +6,13 @@
 /*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 15:54:29 by mcoppola          #+#    #+#             */
-/*   Updated: 2023/01/06 16:42:41 by mcoppola         ###   ########.fr       */
+/*   Updated: 2023/01/18 17:29:40 by mcoppola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_bzero(char *s, int n)
+#include "libft.h"
+
+void	ft_bzero(void *s, int n)
 {
-	while (n > 0)
-	{
-		s[n - 1] = '\0';
-		n--;
-	}
+	ft_memset(s, '\0', n);
 }
