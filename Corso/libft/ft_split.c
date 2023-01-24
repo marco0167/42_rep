5e3e82ed5112fa2532a5019e0238962a346b7cc7
marco0167 <marcocoppola01671@gmail.com>
2023-01-24 20:12:15 +0100

    add split

---
+++ Corso/libft/ft_split.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 20:08:01 by mcoppola          #+#    #+#             */
+/*   Updated: 2023/01/24 20:08:03 by mcoppola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	strlen_neg_pos(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+static int	count_nbr_words(const char *str, char c)
+{
+	int	i;
+	int	trigger;
+
+	i = 0;
+	trigger = 0;
+	while (*str)
+	{
+		if (*str != c && trigger == 0)
+		{
+			trigger = 1;
+			i++;
+		}
+		else if (*str == c)
+			trigger = 0;
+		str++;
+	}
+	return (i);
+}
+
+static char	*copy_words(const char *str, int start, int end)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc((end - start + 1) * sizeof(char));
+	while (start < end)
+	{
+		word[i] = str[start];
+		i++;
+		start++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	int		i;
+	int		start;
+	int		j;
+
+	split = malloc((count_nbr_words(s, c) + 1) * sizeof(char *));
+	if (!s || !split)
+		return (0);
+	start = -1;
+	i = 0;
+	j = 0;
+	while (i <= strlen_neg_pos(s))
+	{
+		if (s[i] != c && start < 0)
+			start = i;
+		else if ((s[i] == c || i == strlen_neg_pos(s)) && start >= 0)
+		{
+			split[j++] = copy_words(s, start, i);
+			start = -1;
+		}
+		i++;
+	}
+	split[j] = 0;
+	return (split);
+}
--- Corso/libft/libft.h
+++ Corso/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 18:40:28 by mcoppola          #+#    #+#             */
-/*   Updated: 2023/01/24 20:06:46 by mcoppola         ###   ########.fr       */
+/*   Updated: 2023/01/24 20:11:52 by mcoppola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
