d7c3b3abde29a0f2aaf99d31c33a51ea9990e214
marco0167 <marcocoppola01671@gmail.com>
2023-01-28 19:02:14 +0100

    final push

--- Corso/libft/ft_strlen.c
+++ Corso/libft/ft_strlen.c
@@ -6,12 +6,11 @@
 /*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 16:42:24 by mcoppola          #+#    #+#             */
-/*   Updated: 2023/01/26 07:56:01 by mcoppola         ###   ########.fr       */
+/*   Updated: 2023/01/28 17:34:32 by mcoppola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t	ft_strlen(char *s)
 {
@@ -22,8 +21,3 @@ size_t	ft_strlen(char *s)
 		l++;
 	return (l);
 }
-
-// int	main(void)
-// {
-// 	printf("%zu\n", ft_strlen("123"));
-// }
