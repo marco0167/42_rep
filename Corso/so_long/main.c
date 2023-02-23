02b004b93bce7f0c5b7ac2bbf7f4d21324be42b9
marco0167 <marcocoppola01671@gmail.com>
2023-02-23 10:35:46 +0100

    porcoddio

---
+++ Corso/so_long/main.c
@@ -0,0 +1,26 @@
+#include <mlx.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+int	main(void)
+{
+	void	*mlx;
+	t_data	img;
+
+	mlx = mlx_init();
+	img.img = mlx_new_image(mlx, 1920, 1080);
+
+	/*
+	** After creating an image, we can call `mlx_get_data_addr`, we pass
+	** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
+	** then be set accordingly for the *current* data address.
+	*/
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+}
