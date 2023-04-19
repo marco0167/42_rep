0d6d01f98ff2b1e6157be2ef255198b64afa9230
Marco <74144941+marco0167@users.noreply.github.com>
2023-04-19 16:58:09 +0000

    upgrade the pos

--- Corso/push_swap/src/moves/ft_moves.c
+++ Corso/push_swap/src/moves/ft_moves.c
@@ -6,11 +6,39 @@
 /*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 17:42:36 by mcoppola          #+#    #+#             */
-/*   Updated: 2023/04/18 13:09:48 by mcoppola         ###   ########.fr       */
+/*   Updated: 2023/04/19 16:20:45 by mcoppola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+void	ft_upg_pos_a(t_stacks *stacks)
+{
+	t_node	*current;
+	int		pos_cntr;
+
+	pos_cntr = 0;
+	current = stacks->stack_a;
+	while (current != NULL)
+	{
+		current->pos = pos_cntr++;
+		current = current->next;
+	}
+}
+
+void	ft_upg_pos_b(t_stacks *stacks)
+{
+	t_node	*current;
+	int		pos_cntr;
+
+	pos_cntr = 0;
+	current = stacks->stack_b;
+	while (current != NULL)
+	{
+		current->pos = pos_cntr++;
+		current = current->next;
+	}
+}
 
 void	ft_moves(t_stacks *stacks)
 {
