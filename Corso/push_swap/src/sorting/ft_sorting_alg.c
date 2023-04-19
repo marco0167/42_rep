eb9308e944c84f0eea8bea13e83a60f0af9636d8
Marco <74144941+marco0167@users.noreply.github.com>
2023-04-19 16:58:49 +0000

    sorting alg goes SEGMENTATION FAULT (core dumped)

--- Corso/push_swap/src/sorting/ft_sorting_alg.c
+++ Corso/push_swap/src/sorting/ft_sorting_alg.c
@@ -6,11 +6,13 @@
 /*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 12:53:50 by mcoppola          #+#    #+#             */
-/*   Updated: 2023/04/19 11:21:07 by mcoppola         ###   ########.fr       */
+/*   Updated: 2023/04/19 16:56:52 by mcoppola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+void	ft_find_cheaper(t_stacks *stacks);
 
 void	ft_print_cost(t_node **head, char *text)//funzione da eliminare
 {
@@ -21,21 +23,84 @@ void	ft_print_cost(t_node **head, char *text)//funzione da eliminare
 	printf("%s\n", text);
 	while (current != NULL)
 	{
-		printf("COSTO DI %d = %d--PrevNum: %d\n", current->data, current->cost, current->prev_num);
+		printf("COSTO DI %d = %d--PrevNum: %d--- POS-%d\n", current->data, current->cost, current->prev_num, current->pos
+		);
 		current = current->next;
 	}
 	// printf("COSTO DI %d = %d\n", current->data, current->cost);
 }
 
-// void	ft_find_prev_nb()
+void	ft_mov_alg(t_node *curr, t_node *prev, t_stacks *stacks)
+{
+	if (curr->pos != 0 && prev->pos != 0)
+	{
+		if (curr->pos > (stacks->len_a / 2) && prev->pos >
+			(stacks->len_b / 2))
+			ft_reverse_rotate_rrr(stacks);
+		ft_find_cheaper(stacks);
+	}
+	else if (curr->pos == 0 && prev->pos == 0)
+		ft_push_b(stacks);
+	else
+	{
+		if (curr->pos != 0)
+			if (curr->pos > (stacks->len_a / 2))
+				ft_reverse_rotate_a(stacks);
+			else
+				ft_rotate_a(stacks);
+		if (prev->pos != 0)
+			if (prev->pos > (stacks->len_b / 2))
+				ft_reverse_rotate_b(stacks);
+			else
+				ft_rotate_b(stacks);
+		ft_find_cheaper(stacks);
+	}
+
+}
+
+t_node	*ft_search_prev_num(t_node **head, int data)
+{
+	t_node	*curr;
+
+	curr = *head;
+	while (curr != NULL)
+	{
+		if (curr->data == data)
+			return (curr);
+		curr = curr->next;
+	}
+}
+
+void	ft_find_cheaper(t_stacks *stacks)
+{
+	t_node	*current;
+	t_node	*cheaper;
+
+	current = stacks->stack_a;
+	cheaper = stacks->stack_a;
+	while (current != NULL)
+	{
+		if (current->cost < cheaper->cost)
+			cheaper = current;
+		current = current->next;
+	}
+	ft_mov_alg(cheaper, ft_search_prev_num(&stacks->stack_b, cheaper->prev_num),
+				stacks);
+	printf("\nCheaper %d - %d - %d -%d\n", cheaper->data, cheaper->cost, cheaper->prev_num, cheaper->pos);
+}
 
 void	ft_sorting_alg(t_stacks *stacks)
 {
 	ft_push_b(stacks);
-	ft_push_b(stacks);
-	ft_push_b(stacks);
-	ft_push_b(stacks);
 	ft_init_cost(&stacks->stack_a, &stacks->stack_a, stacks);
+	while (stacks->stack_a != NULL)
+		ft_find_cheaper(stacks);
 	ft_print_cost(&stacks->stack_a, "----STACK  A----");
 	ft_print_cost(&stacks->stack_b, "----STACK  B----");
+	// 	ft_find_cheaper(stacks);
+	// ft_print_cost(&stacks->stack_a, "----STACK  A----");
+	// ft_print_cost(&stacks->stack_b, "----STACK  B----");
+	// 	ft_find_cheaper(stacks);
+	// ft_print_cost(&stacks->stack_a, "----STACK  A----");
+	// ft_print_cost(&stacks->stack_b, "----STACK  B----");
 }
