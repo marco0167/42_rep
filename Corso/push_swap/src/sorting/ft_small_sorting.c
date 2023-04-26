9776d5c5361277d8901341bee12452b0adae774f
Marco <74144941+marco0167@users.noreply.github.com>
2023-04-26 10:43:08 +0000

    add condition

--- Corso/push_swap/src/sorting/ft_small_sorting.c
+++ Corso/push_swap/src/sorting/ft_small_sorting.c
@@ -6,7 +6,7 @@
 /*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 18:23:45 by mcoppola          #+#    #+#             */
-/*   Updated: 2023/04/20 18:40:50 by mcoppola         ###   ########.fr       */
+/*   Updated: 2023/04/26 10:34:34 by mcoppola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,27 @@ void	ft_small_sorting(t_stacks *stacks)
 {
 	t_node	*head;
 
-	head = stacks->stack_a;
-	if (head->data > head->next->data)
+	if (ft_is_alrd_sorted(stacks) == 1)
 	{
-		if (head->next->data > head->next->next->data)
+		head = stacks->stack_a;
+		if (head->data > head->next->data)
 		{
-			ft_rotate_a(stacks);
+			if (head->next->data > head->next->next->data)
+			{
+				ft_rotate_a(stacks);
+				ft_swap_a(stacks);
+			}
+			else if (head->data < head->next->next->data)
+				ft_swap_a(stacks);
+			else
+				ft_rotate_a(stacks);
+		}
+		else if (head->data > head->next->next->data)
+			ft_reverse_rotate_a(stacks);
+		else
+		{
+			ft_reverse_rotate_a(stacks);
 			ft_swap_a(stacks);
 		}
-		else if (head->data < head->next->next->data)
-			ft_swap_a(stacks);
-		else
-			ft_rotate_a(stacks);
-	}
-	else if (head->data > head->next->next->data)
-		ft_reverse_rotate_a(stacks);
-	else
-	{
-		ft_reverse_rotate_a(stacks);
-		ft_swap_a(stacks);
 	}
 }
