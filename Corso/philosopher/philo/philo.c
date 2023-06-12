f600bee466ace83e6e01e29911d829791c87be5e
Marco <74144941+marco0167@users.noreply.github.com>
2023-06-12 16:54:54 +0000

    esempio thread

--- Corso/philosopher/philo/philo.c
+++ Corso/philosopher/philo/philo.c
@@ -1,43 +1,67 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mcoppola <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/06 18:18:04 by mcoppola          #+#    #+#             */
-/*   Updated: 2023/06/06 18:55:02 by mcoppola         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************
 
-#include "philo.h"
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
 
-void	*print_message(void *ptr, int *var)
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int numero = 0;
+
+pthread_mutex_t mutex;
+
+void    *process(void *arg)
 {
-	int		id;
-	int		val;
-
-	id = (int)ptr;
-	var++;
-	val = var;
-	printf("Thread %d\n", val);
-	pthread_exit(NULL);
+    int num = *(int *)arg;
+    int *result = malloc(sizeof(int));
+    
+    *result = num;
+    int i;
+    for (i = 0; i < 10000000; i++)
+    {
+        pthread_mutex_lock(&mutex);
+        numero++;
+        pthread_mutex_unlock(&mutex);
+    }
+    
+    printf("Thread %d\n", num);
+    return ((void *) result);
 }
 
-int	main(void)
+int main()
 {
-	pthread_t	thread[5];
-	int rc, i, var=0;
-
-	for (i = 0; i < 5; i++)
-	{
-		rc = pthread_create(&thread[i], NULL, print_message, ((void *)i, &var));
-		printf("Creating thread! value %d\n", var);
-		if (rc)
-		{
-			printf("Error: unable to create thread, %d\n", rc);
-			exit(-1);
-		}
-	}
-	return (0);
+    pthread_t   th[2];
+    int i;
+    
+    int *res;
+    
+    pthread_mutex_init(&mutex, NULL);
+    
+    for (i = 0; i < 2; i++)
+    {
+        int *num = malloc(sizeof(int));
+        *num = i;
+        if (pthread_create(&th[i], NULL, &process, num) != 0)
+        {
+            return (1);
+        }
+    }
+    
+    for (i = 0; i < 2; i++)
+    {
+        if(pthread_join(th[i], (void **) &res) != 0)
+        {
+            return (1);
+        }
+        printf("result %d\n", *res);
+    }
+    
+    pthread_mutex_destroy(&mutex);
+    printf("NUMERO %d\n", numero);
+    
+    return 0;
 }
