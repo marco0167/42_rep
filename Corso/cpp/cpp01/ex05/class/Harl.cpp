d950df1a283446e456c3266f1761e01eb894a5af
marco <marcocoppola01671@gmail.com>
2024-04-30 16:31:40 +0200

    fix: changed message

--- Corso/cpp/cpp01/ex05/class/Harl.cpp
+++ Corso/cpp/cpp01/ex05/class/Harl.cpp
@@ -6,22 +6,25 @@ Harl::~Harl(){}
 
 void	Harl::debug(void)
 {
-	std::cout << "DEBUG MESSAGE" << std::endl;
+	std::cout << "[ DEBUG ]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup "
+		"burger.\nI really do!\n" << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "INFO MESSAGE" << std::endl;
+	std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money.\nYou didn’t put "
+		"enough bacon in my burger! If you did, I wouldn’t be asking for more!\n" << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "WARNING MESSAGE" << std::endl;
+	std::cout << "[ WARNING ]\nI think I deserve to have some extra bacon for free.\nI’ve been coming for "
+		"years whereas you started working here since last month.\n" << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "ERROR MESSAGE" << std::endl;
+	std::cout << "[ ERROR ]\nThis is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain(string level) {
