667d549fc5cfef32b2d312427e150fafc6da8a32
Virtual machine 42 <marcocoppola01671@gmail.com>
2024-11-23 18:34:46 +0100

    inception final

--- Corso/inception/srcs/requirements/mariadb/configurations/entrypoint.sh
+++ Corso/inception/srcs/requirements/mariadb/configurations/entrypoint.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-#--------------mariadb start--------------#
-service mariadb start # start mariadb
-sleep 5 # wait for mariadb to start
+service mariadb start 
+sleep 5
 
-#--------------mariadb config--------------#
-# Create database if not exists
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 
-# Create user if not exists
-mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 
-# Grant privileges to user
-mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO \`${DB_USER}\`@'%';"
 
-# Flush privileges to apply changes
 mariadb -e "FLUSH PRIVILEGES;"
 
-#--------------mariadb restart--------------#
-# Shutdown mariadb to restart with new config
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
 
-# Restart mariadb with new config in the background to keep the container running
 mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
--- Corso/inception/srcs/requirements/wordpress/configurations/entrypoint.sh
+++ Corso/inception/srcs/requirements/wordpress/configurations/entrypoint.sh
@@ -1,82 +1,46 @@
 #!/bin/bash
 
-#---------------------------------------------------#
-#                   WP Installation                 #
-#---------------------------------------------------#
-
-# Install WP-CLI
-echo "[======== Installing WP-CLI ========]"
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-echo "[======== WP-CLI Installed Successfully ========]"
 
-# Set up WordPress Directory
-echo "[======== Setting Up WordPress Directory ========]"
 cd /var/www/wordpress
 chmod -R 755 /var/www/wordpress/
 chown -R www-data:www-data /var/www/wordpress
-echo "[======== WordPress Directory Configured ========]"
 
-#---------------------------------------------------#
-#                 MariaDB Connectivity              #
-#---------------------------------------------------#
-
-# Function to check if MariaDB container is up
 ping_mariadb_container() {
     nc -zv mariadb 3306 > /dev/null
     return $?
 }
 
-# Wait for MariaDB to be available
-echo "[======== Checking MariaDB Status ========]"
 start_time=$(date +%s)
 end_time=$((start_time + 20))
 
 while [ $(date +%s) -lt $end_time ]; do
     if ping_mariadb_container; then
-        echo "[======== MARIADB IS UP AND RUNNING ========]"
+        echo "[======== MARIADB UP AND RUNNING ========]"
         break
     else
-        echo "[======== WAITING FOR MARIADB TO START... ========]"
+        echo "[======== WAITING FOR MARIADB START... ========]"
         sleep 1
     fi
 done
 
 if [ $(date +%s) -ge $end_time ]; then
-    echo "[======== MARIADB IS NOT RESPONDING ========]"
+    echo "[======== MARIADB NOT RESPONDING ========]"
     exit 1
 fi
 
-#---------------------------------------------------#
-#                  WP Configuration                 #
-#---------------------------------------------------#
-
-# Download and set up WordPress
-echo "[======== Downloading WordPress Core ========]"
 wp core download --allow-root
 
-echo "[======== Configuring WordPress ========]"
-wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
+wp core config --dbhost=mariadb:3306 --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --allow-root
 
-echo "[======== Installing WordPress ========]"
-wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
+wp core install --url="mcoppola.42.fr" --title="inception" --admin_user="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
 
-echo "[======== Creating a New User ========]"
-wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U_ROLE" --allow-root
-echo "[======== WordPress Installation Completed ========]"
+wp user create "$WP_USERNAME" "$WP_USEREMAIL" --user_pass="$WP_USERPASSWORD" --role="$WP_USERROLE" --allow-root
 
-#---------------------------------------------------#
-#                 PHP Configuration                 #
-#---------------------------------------------------#
-
-# Adjust PHP-FPM settings
-echo "[======== Configuring PHP-FPM ========]"
 sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
 
-# Ensure PHP-FPM directory exists
 mkdir -p /run/php
 
-# Start PHP-FPM service
-echo "[======== Starting PHP-FPM Service ========]"
 /usr/sbin/php-fpm7.4 -F
